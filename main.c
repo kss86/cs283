adf7a70fe85c43210c2cc8901abf76534d86bd47
Your Name <you@example.com>
2014-03-01 00:29:06 -0500

    added some testing and trying to get the STAT to update in a loop

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ function monitor {
 		else whatever
 	sleep timeinterval
 }*/
-
+#include <openssl/md5.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,28 +26,60 @@ function monitor {
 #include <time.h>
 
 //global temp storage directory to keep the files
-char *tmpdir;
-tmpdir="./tmpdir/";
+char tmpdir[100]="tmpdir/\0";
 
 
-void monitorfile(char *filename) {
+void monitorfile(char *filename, int wait) {
         struct stat fileStat;
         if(stat(filename,&fileStat) < 0) {
-                printf("Error: Cannot stat %s", filename);
-                return 1;}
-	char *path;
+                printf("Error: Cannot stat %s\n", filename);
+                }
+	char path[100];
 	strcpy(path, tmpdir);                
 	strcat(path, filename);
-	FILE *f;
+	char content[100];
+	strcpy(content, path);
+	strcat(content, ".content");
+	strcat(path, ".stat");
+	FILE *f,*fp,*fp2;
 	f = fopen(path, "w");
-	fprintf(f,"Last Access: %s\n", fileStat.st_atime);	
-	fprintf(f,"Last Modified: %s\n", fileStat.st_mtime);
-	fprintf(f,"Last Status Change: %s\n", fileStat.st_ctime);
-	fprintf(f,"Mode: %s\n", fileStat.st_mode);
-	fprintf(f,"User ID: %s\n", fileStat.st_uid);
-	fprintf(f,"Group ID:%s\n", fileStat.st_gid);
-	fprintf(f,"Size: %s\n", fileStat.st_size);
-	f = close(f);
+	fprintf(f,"Last Access: %d\n", fileStat.st_atime);	
+	fprintf(f,"Last Modified: %d\n", fileStat.st_mtime);
+	static time_t cmon = fileStat.st_mtime;
+	fprintf(f,"Last Status Change: %d\n", fileStat.st_ctime);
+	
+	fprintf(f,"Mode: %d\n", fileStat.st_mode);
+	fprintf(f,"User ID: %d\n", fileStat.st_uid);
+	fprintf(f,"Group ID: %d\n", fileStat.st_gid);
+	fprintf(f,"Size: %d\n", fileStat.st_size);
+	fp = fopen(filename, "r");
+	fp2 = fopen(content, "w");
+	char line[128];
+	while( fgets(line,sizeof line, fp) != NULL )
+        {
+            fputs(line, fp2);
+        }
+        fclose(fp);
+
+	fclose(f);
+	struct stat fileStatNEW;	
+	while (1) {
+		int x;
+		x = stat(filename,&fileStatNEW);
+		if(x < 0) {
+                		printf("Error: Cannot stat %s :: error code: %i \n", filename, x);
+               		 	}
+		if (cmon != &fileStatNEW.st_mtime) { 
+			//file attrs have changed, also sent email of attr diff
+			printf("File Changed!!");
+			
+			} 
+		else {printf("Check passed %d : %d\n", cmon, fileStatNEW.st_mtime);
+
+		
+		}
+	sleep(wait);
+	}	
 
 
 }
@@ -60,7 +92,7 @@ int main(int argc, char *argv[] ) {
 //we can discuss
 
 
-monitorfile("./tmpdir/test");
+monitorfile("test", 3);
 
 
 }
